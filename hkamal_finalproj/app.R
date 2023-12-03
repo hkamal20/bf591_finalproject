6d23ac90fb987ec865c62bf20e1a0d8f8ad783d1
Haniya Kamal <hkamal@scc-gf1.scc.bu.edu>
2023-12-03 14:34:03 -0500

    added summary and datatable for samples subtab

--- hkamal_finalproj/app.R
+++ hkamal_finalproj/app.R
@@ -8,10 +8,11 @@
 #
 
 library(shiny)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  titlePanel("Haniya Kamal Bf591 Final Project"),
+  titlePanel("Haniya Kamal BF591 Final Project"),
   sidebarLayout(
     sidebarPanel(
       fileInput('csv_file', label = paste0('Upload your file')),
@@ -20,9 +21,9 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel("Samples",
           tabsetPanel(
-            tabPanel("Summary", verbatimTextOutput("summary")),
-            tabPanel("Table", tableOutput("table")),
-            tabPanel("Plots",
+            tabPanel("Summary", tableOutput("summary")), #Tab with a summary of the table that includes a summary of the type and values in each column
+            tabPanel("Table", DTOutput("table")), #Tab with a data table displaying the sample information, with sortable columns
+            tabPanel("Plots", #Tab with histograms, density plots, or violin plots of continuous variable3
               plotOutput("plot1"),
               plotOutput("plot2"),
               plotOutput("plot3")
@@ -56,19 +57,70 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
+#
 server <- function(input, output) {
+  
+  load_data <- reactive({ #CSV file can be uploaded 
+    req(input$csv_file)
+    data <- input$csv_file
+    if (is.null(data)) {
+      return(NULL)
+    }
+    d<-read.csv(data$datapath, header = TRUE)
+    return(d)
+  })
+  
+  datatable1 <- function(dataf) { #making the datatable for samples tab
+    df <- dataf 
+    return(df)
+  }
+  
+  summarytable <- function(dataf) {
+    notsum <- datatable1(dataf)
+    df <- notsum[, !(names(dataf) %in% c("Sample.Name", "Sample.Geo.Accession"))]
+    
+    #function to calculate Mean (sd) or Distinct Values
+    summarization <- function(x) {
+      # Remove NAs and then calculate mean and sd for numeric columns
+      x <- x[!is.na(x)]
+      
+      if (is.numeric(x)) {
+        mean_val <- mean(x)
+        sd_val <- sd(x)
+        return(paste(round(mean_val, 2), " (+/- ", round(sd_val, 2), ")"))
+      } else {
+        # Count distinct values for non-numeric columns
+        distinct_values <- length(unique(x))
+        return(paste(distinct_values, " distinct values"))
+      }
+    }
+    
+    # Create a logical vector for character columns
+    is_character <- sapply(df, is.character)
+    
+    # Conditionally set values based on data type
+    type_values <- ifelse(is_character, "character", sapply(df, class))
+    
+    # Create the final data frame
+    d_sum <- data.frame(
+      Columns = names(df),
+      Type = type_values,
+      Mean_or_Distinct_Value = ifelse(is_character, "Neurologically Normal, Huntington's Disease", sapply(df, summarization))
+    )
+    
+    return(d_sum)
+  }
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$summary <- renderTable({ #pulling the summary table from the function, part of samples tabset
+    summary_df <- load_data()
+    summarytable(summary_df)
+    
+  })
+  
+  output$table <- renderDT({ #pulling the table from the Samples tabset
+    data2 <- load_data()             #table will be sortable
+    datatable1(data2) #loading the sample table 
+  })
 }
 
 # Run the application 
